bc958a7606f49f8449fd67cf3db7fe6e805b4e33
Alexey Volobuev <17valexl@gmail.com>
2016-02-26 07:09:35 +0000

    Add feature test case for adding new company. Create easy views for this logic. Add locales for new views

--- spec/features/investor/adding_company_spec.rb
+++ spec/features/investor/adding_company_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature 'adding company' do
   before(:all) do
     User.destroy_all
+    Company.destroy_all
   end
 
   let(:investor) { FactoryGirl.create(:user, password: '11223344', password_confirmation: '11223344')}
@@ -17,7 +18,19 @@ RSpec.feature 'adding company' do
     
     click_button "Login"
 
-    click_link("Copmanies")
+    click_link("Companies")
+    click_link("Add new company")
+
+    within("form") do
+      fill_in "company_name", :with => "First company" 
+    end
+
+    click_button "Submit"
+
+    expect(page.current_path).to eq(investor_companies_path)
+    expect(page).to have_text("First company")
+    expect(page).to have_text("Pending")
+
   end
 
 end
